443a4f575db0c96fb29f98d203a393059a068aee
Juan Mendez <som.felipe66@gmail.com>
2019-04-24 09:43:01 -0400

    modified iterator in executable to count all possible configurations of the free

--- doublewell/run.sh
+++ doublewell/run.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+
+
 for n_bound in {4..8}; do
-	export nbound="$(echo $n_bound | awk '{printf("%d", $1-1)}')"
-	export ibound="$(echo $n_bound | awk '{printf("%.4f", 0.719643*$1)}')"
-	for n_pos in {1..${nbound}}; do
-    	export inpos="$(echo $n_pos ${ibound} ${nbound}| awk '{printf("%.4f", 2*$2*$1/(2*$3) -$2/($3))}')"
-    	jdftx -i totalE.in
-	done
+    export nbound="$(echo $n_bound | awk '{printf("%d", $1-1)}')"
+    export ibound="$(echo $n_bound | awk '{printf("%.4f", 0.719643*$1)}')"
+    echo “Outside loop: ${nbound}”
+    for (( n_pos=1; n_pos<=${nbound}; n_pos++ )); do
+        export inpos="$(echo $n_pos ${ibound} ${nbound}| awk '{printf("%.4f", 2*$2*$1/(2*$3) -$2/($3))}')"
+
+         jdftx -i totalE.in
+    done
 done
